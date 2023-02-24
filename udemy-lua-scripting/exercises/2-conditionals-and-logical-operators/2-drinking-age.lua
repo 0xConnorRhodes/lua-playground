19dd28958c46428dc13d24138deb2a5e0a55406e
Connor Rhodes <connor@rhodes.contact>
2023-02-24 13:57:59 -0600

    added drinking age calculator

---
+++ udemy-lua-scripting/exercises/2-conditionals-and-logical-operators/2-drinking-age.lua
@@ -0,0 +1,10 @@
+--check if the user's age is >= 18
+--if not, print you can't drink else print you can drink
+
+age = 17
+
+if age <= 17 then
+	print("Sorry. You are not old enough to drink")
+else
+	print("You are old enough to drink.")
+end
