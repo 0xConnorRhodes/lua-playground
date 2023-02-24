b18a461c12ac470abc48630be33a20f5374ad1f0
Connor Rhodes <connor@rhodes.contact>
2023-02-24 13:52:36 -0600

    added or statement to tie Ken and Ryu together

--- udemy-lua-scripting/exercises/2-conditionals-and-logical-operators/1-street-fighter-special-moves.lua
+++ udemy-lua-scripting/exercises/2-conditionals-and-logical-operators/1-street-fighter-special-moves.lua
@@ -1,7 +1,8 @@
 fighter_name = "Chun Li"
 fighter_name_lower = string.lower(fighter_name)
 
-if fighter_name_lower == "ryu" then
+if fighter_name_lower == "ryu" or 
+   fighter_name_lower == "ken" then
 	attack_move = "Hadouken"
 elseif fighter_name_lower == "chun li" then
 	attack_move = "Lightning Kick"
@@ -9,8 +10,6 @@ elseif fighter_name_lower == "guile" then
 	attack_move = "Sonic Boom"
 elseif fighter_name_lower == "honda" then
 	attack_move = "Hundred Hand Slap"
-elseif fighter_name_lower == "ken" then
-	attack_move = "Hadouken"
 elseif fighter_name_lower == "blanka" then
 	attack_move = "Electric Shock"
 end
