85fc3323ebb01bcbe15cfa40dca670af4a49d358
Connor Rhodes <connor@rhodes.contact>
2023-02-24 13:38:07 -0600

    added street fighter special move script using elseif

---
+++ udemy-lua-scripting/exercises/2-conditionals-and-logical-operators/1-street-fighter-special-moves.lua
@@ -0,0 +1,17 @@
+fighter_name = "Chun Li"
+
+if fighter_name == "Ryu" then
+	attack_move = "Hadouken"
+elseif fighter_name == "Chun Li" then
+	attack_move = "Lightning Kick"
+elseif fighter_name == "Guile" then
+	attack_move = "Sonic Boom"
+elseif fighter_name == "Honda" then
+	attack_move = "Hundred Hand Slap"
+elseif fighter_name == "Ken" then
+	attack_move = "Hadouken"
+elseif fighter_name == "Blanka" then
+	attack_move = "Electric Shock"
+end
+
+print(fighter_name.." attacks with "..attack_move)
