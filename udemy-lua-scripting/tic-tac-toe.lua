400f9c25f65e97d4f4ff3800f34711397721e47e
Connor Rhodes <connor@rhodes.contact>
2024-05-03 14:12:41 -0700

    refactor showBoard() to have no side effects

--- udemy-lua-scripting/tic-tac-toe.lua
+++ udemy-lua-scripting/tic-tac-toe.lua
@@ -1,7 +1,9 @@
 function createBoard()
     local board = {}
+    -- loop to create 3 columns of rows
     for i = 1, 3 do
         board[i] = {}
+        -- loop to create 3 rows
         for j = 1, 3 do
             board[i][j] = ' '
         end
@@ -10,10 +12,24 @@ function createBoard()
 end
 
 function showBoard(board)
-  print('   1  2  3 ')
-  for i = 1, 3 do
-    print(i..' ['..board[i][1]..']['..board[i][2]..']['..board[i][3]..']')
-  end
+  local output = {}
+  output.topRow = '   1  2  3 '
+    for i = 1, 3 do
+      -- build each row in the form of 1 [ ] [ ] [ ]
+      output[i] = i..' ['..board[i][1]..']['..board[i][2]..']['..board[i][3]..']'
+    end
+  return output.topRow.."\n"..output[1].."\n"..output[2].."\n"..output[3]
 end
 
-showBoard(createBoard())
+function checkWinner(board)
+end
+
+function checkBoardFull(board)
+end
+
+-- program flow
+
+local board = createBoard()
+
+print(showBoard(board))
+
