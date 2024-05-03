f07e348d91ba9e3d25133a4af7e798a76e265f34
Connor Rhodes <connor@rhodes.contact>
2024-05-03 15:10:22 -0700

    updated checkWinner() functions and added type annotation comments

--- udemy-lua-scripting/tic-tac-toe.lua
+++ udemy-lua-scripting/tic-tac-toe.lua
@@ -1,17 +1,17 @@
-function createBoard()
+function createBoard() -- returns empty board table
     local board = {}
     -- loop to create 3 columns of rows
-    for i = 1, 3 do
-        board[i] = {}
+    for row = 1, 3 do
+        board[row] = {}
         -- loop to create 3 rows
-        for j = 1, 3 do
-            board[i][j] = ' '
+        for column = 1, 3 do
+            board[row][column] = ' '
         end
     end
     return board
 end
 
-function showBoard(board)
+function showBoard(board) -- returns multiline string of the board
   local output = {}
   output.topRow = '   1  2  3 '
     for i = 1, 3 do
@@ -28,7 +28,7 @@ function showBoard(board)
   )
 end
 
-function placeMarker(board, player, row, column)
+function placeMarker(board, player, row, column) -- returns new board
   board[row][column] = tostring(player)
   return board
 end
@@ -36,12 +36,31 @@ end
 function checkWinner(board)
 end
 
-function checkBoardFull(board)
+function checkBoardFull(board) -- returns boolean
+  for row = 1, #board do
+    for column = 1, #board[row] do
+      if board[row][column] == ' ' then
+        return false
+      end
+    end
+  end
+  -- this works because one the function hits a `return` it stops 
+  -- so it never reaches the below unless the if statement is never true
+  return true
 end
 
 -- program flow
 
 local board = createBoard()
+board = placeMarker(board, 'X', 1, 1)
 board = placeMarker(board, 'X', 1, 2)
+board = placeMarker(board, 'X', 1, 3)
+board = placeMarker(board, 'X', 2, 1)
+board = placeMarker(board, 'X', 2, 2)
+board = placeMarker(board, 'X', 2, 3)
+board = placeMarker(board, 'X', 3, 1)
+board = placeMarker(board, 'X', 3, 2)
+board = placeMarker(board, 'X', 3, 3)
 
 print(showBoard(board))
+print(checkBoardFull(board))
